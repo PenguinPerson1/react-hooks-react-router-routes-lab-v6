361677a2e1e490de12bcebedda03e7c356098600
Annania Plossl <nia.plossl@gmail.com>
2024-04-10 10:39:54 -0400

    completed the lab

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const params = useParams();
+  const [movieData,setMovieData] = useState({
+    title: "",
+    time: "",
+    genres: []
+  });
+  useEffect(()=>{
+    fetch(`http://localhost:4000/movies/${params.id}`)
+    .then(r=>r.json())
+    .then(data => setMovieData(data))
+  },[])
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movieData.title}</h1>
+        <p>{movieData.time}</p>
+        {movieData.genres.map((genre, i) => <span key={i}>{genre}</span>)}
       </main>
     </>
   );
